668716f500b873477d4ddda13c28c88e11561cd3
denkhaus <peristaltic@gmx.net>
2019-05-31 14:05:13 +0200

    add ensure rancher, rancher-compose

--- rancher/rancher.go
+++ rancher/rancher.go
@@ -2,15 +2,34 @@ package rancher
 
 import (
 	"fmt"
+	"os/exec"
 
+	pipe "gopkg.in/pipe.v2"
+
+	"github.com/denkhaus/logging"
 	"github.com/denkhaus/magelib/common"
+	"github.com/magefile/mage/mg"
 	"github.com/magefile/mage/sh"
+)
+
+const (
+	//TODO:  make tis customizable
+	VersionRancherCLI     = "v0.6.10"
+	VersionRancherCompose = "v0.12.5"
 )
 
 var (
 	RancherOut = sh.OutCmd("rancher")
 	ComposeOut = sh.OutCmd("rancher-compose")
 )
+
+func RancherURL(version string) string {
+	return fmt.Sprintf("https://releases.rancher.com/cli/%s/rancher-linux-amd64-%s.tar.gz", version, version)
+}
+
+func RancherComposeURL(version string) string {
+	return fmt.Sprintf("https://releases.rancher.com/compose/%s/rancher-compose-linux-amd64-%s.tar.gz", version, version)
+}
 
 func ContainerNameByLabel(host, label string) string {
 	label = fmt.Sprintf("label=%s", label)
@@ -26,7 +45,60 @@ func ContainerNameByLabel(host, label string) string {
 	return name
 }
 
+func EnsureRancher() error {
+	if _, err := exec.LookPath("rancher"); err != nil {
+		logging.Info("install rancher CLI")
+		return InstallRancher(VersionRancherCLI)
+	}
+
+	return nil
+}
+
+func InstallRancher(version string) error {
+	p := pipe.Script(
+		pipe.Line(
+			pipe.Exec("curl", RancherURL(version)),
+			pipe.Exec("sudo", "tar", "--strip-components", "2", "-C", "/usr/bin/", "-xzf", "-"),
+		),
+		pipe.Exec("ls", "-la", "/usr/bin/rancher"),
+	)
+
+	output, err := pipe.CombinedOutput(p)
+	if len(output) > 0 {
+		logging.Info(string(output))
+	}
+
+	return err
+}
+
+func EnsureRancherCompose() error {
+	if _, err := exec.LookPath("rancher-compose"); err != nil {
+		logging.Info("install rancher-compose")
+		return InstallRancherCompose(VersionRancherCompose)
+	}
+
+	return nil
+}
+
+func InstallRancherCompose(version string) error {
+	p := pipe.Script(
+		pipe.Line(
+			pipe.Exec("curl", RancherComposeURL(version)),
+			pipe.Exec("sudo", "tar", "--strip-components", "2", "-C", "/usr/bin/", "-xzf", "-"),
+		),
+		pipe.Exec("ls", "-la", "/usr/bin/rancher-compose"),
+	)
+
+	output, err := pipe.CombinedOutput(p)
+	if len(output) > 0 {
+		logging.Info(string(output))
+	}
+
+	return err
+}
+
 func RancherCompose(moduleDir, stack string) error {
+	mg.Deps(EnsureRancherCompose)
 	err := common.InDirectory(moduleDir, func() error {
 		return sh.RunV("rancher-compose", "-p", stack, "up", "-d", "--force-upgrade")
 	})
@@ -35,6 +107,7 @@ func RancherCompose(moduleDir, stack string) error {
 }
 
 func RancherComposeWith(env map[string]string, moduleDir, stack string) error {
+	mg.Deps(EnsureRancherCompose)
 	err := common.InDirectory(moduleDir, func() error {
 		return common.RunVWith(env, "rancher-compose", "-p", stack, "up", "-d", "--force-upgrade")
 	})
@@ -43,6 +116,7 @@ func RancherComposeWith(env map[string]string, moduleDir, stack string) error {
 }
 
 func Rancher(moduleDir, stack string) error {
+	mg.Deps(EnsureRancher)
 	err := common.InDirectory(moduleDir, func() error {
 		return sh.RunV("rancher", "up", "-s", stack, "-d", "--force-upgrade")
 	})
@@ -51,6 +125,7 @@ func Rancher(moduleDir, stack string) error {
 }
 
 func RancherWith(env map[string]string, moduleDir, stack string) error {
+	mg.Deps(EnsureRancher)
 	err := common.InDirectory(moduleDir, func() error {
 		return common.RunVWith(env, "rancher", "up", "-s", stack, "-d", "--force-upgrade")
 	})
